6f1217ebd5b2cf4abd316a69a4d196fcbdf535c6
jlcostley <jlcostley@lycos.com>
2015-02-18 16:57:46 -0500

    Matrix inversion functions.  If the inverse has already been calculated, the inverse is retrieved from cache. Initial commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function contains two functions which together 
+## cache the inverse of a matrix
 
-## Write a short comment describing this function
+## The first function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The second function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix or
+## if the inverse has already been calculated, it retrives it from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
